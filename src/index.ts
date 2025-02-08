b070a481048e2f745fc8eb20f9e0a18f4affeec2
John Vorwald <jvorwald@leantechniques.com>
2025-02-08 17:11:12 -0600

    adding the init.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,38 @@
 import { AppDataSource } from "./data-source"
+import { Address } from "./entity/Address";
+import { Catalog } from "./entity/Catalog";
+import { Order } from "./entity/Order";
 import { User } from "./entity/User"
+import { addressgen } from "./generators/addressgen";
+import { cataloggen } from "./generators/cataloggen";
+import { ordergen } from "./generators/ordergen";
+import { usergen } from "./generators/usergen";
+import { pino } from 'pino';
 
-AppDataSource.initialize().then(async () => {
+export const init = () => {
+    AppDataSource.initialize().then(async () => {
+        const logger = pino;
+        const hasUsers = await AppDataSource.createQueryBuilder()
+            .select("user")
+            .from(User, "user")
+            .getOne();
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+        if (hasUsers) { 
+            pino().info("Database already has data, skipping seeding");       
+            return;
+        }
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+        const users = usergen(10);
+        const catalog = cataloggen(50);
+        const addresses = addressgen(users);
+        const orders = ordergen(users, addresses, catalog)
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+        await AppDataSource.manager.save(User, users);
+        await AppDataSource.manager.save(Address, addresses);
+        await AppDataSource.manager.save(Catalog, catalog);
+        await AppDataSource.manager.save(Order, orders);
+        pino().info("Seeding complete");
+    }).catch(error => pino().info(error));
+};
 
-}).catch(error => console.log(error))
+init();
